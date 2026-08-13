0a2c0250122e5bf8ddc2759e43a8211b9a8eb7d9
My Pham Ha Le <my.le@asnet.com.vn>
2026-08-13 17:03:15 +0700

    update import for use-generative-ui hook

--- practice-new-version/src/hooks/index.ts
+++ practice-new-version/src/hooks/index.ts
@@ -1,6 +1,6 @@
 export * from "./use-email-agent";
 export * from "./use-example-suggestions";
-export * from "./use-generative-ui-examples";
+export * from "./use-generative-ui";
 export * from "./use-openai-key";
 export * from "./use-self-managed-threads";
 export * from "./use-shared-inbox";
