e17408cdd0af2a685161a99316f750fa917af3d9
My Pham Ha Le <my.le@asnet.com.vn>
2026-08-25 11:05:06 +0700

    refactor: streamline exports across apps

--- practice-new-version/apps/web/src/constants/index.ts
+++ practice-new-version/apps/web/src/constants/index.ts
@@ -1,7 +1,7 @@
-export { COURSE_LABEL } from "./course";
-export { ERROR_MESSAGE, GENERIC_ERROR_MESSAGE, messageForCode } from "./errors";
-export { STATUS_LABEL, STATUS_TONE } from "./status";
-export { FALLBACK_TONE, TONE } from "./tone";
-export { TOPIC_LABEL, TOPIC_TONE } from "./topic";
-export { URGENCY_LABEL, URGENCY_TONE, URGENCY_VARIANT } from "./urgency";
-export { WORK_TYPE_LABEL } from "./workType";
+export * from "./course";
+export * from "./errors";
+export * from "./status";
+export * from "./tone";
+export * from "./topic";
+export * from "./urgency";
+export * from "./workType";
--- practice-new-version/apps/web/src/types/index.ts
+++ practice-new-version/apps/web/src/types/index.ts
@@ -1,4 +1,4 @@
 export * from "@/types/errors";
-export * from "@/types/tools";
-export * from "@/types/email";
-export * from "@/types/thread";
+export type * from "./email";
+export * from "./errors";
+export type * from "./tools";
