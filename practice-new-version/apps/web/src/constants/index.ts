0ce36c9dd5168eb40abd5533aee49e12083efac1
My Pham Ha Le <my.le@asnet.com.vn>
2026-08-26 09:21:30 +0700

    refactor: update exports in index.ts to use wildcard syntax for shared module

--- practice-new-version/apps/web/src/constants/index.ts
+++ practice-new-version/apps/web/src/constants/index.ts
@@ -1,4 +1,4 @@
-export { COMPOSE_REPLY_ACTION, CUSTOM_CATALOG_ID, TOOL } from "@repo/shared";
+export * from "@repo/shared";
 export * from "./course";
 export * from "./errors";
 export * from "./status";
