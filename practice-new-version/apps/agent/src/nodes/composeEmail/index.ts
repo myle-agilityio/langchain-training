ee2acce7c983fdd81fde22efa5b2bc760eeca5c8
My Pham Ha Le <my.le@asnet.com.vn>
2026-09-14 14:29:23 +0700

    feat: simplify exports in composeEmail index file

--- practice-new-version/apps/agent/src/nodes/composeEmail/index.ts
+++ practice-new-version/apps/agent/src/nodes/composeEmail/index.ts
@@ -1,5 +1,5 @@
-export { checkCompliance } from "./checkCompliance";
-export { requestApproval } from "./requestApproval";
-export { research } from "./research";
-export { afterTriage, triage } from "./triage";
-export { writeDraft } from "./writeDraft";
+export * from "./checkCompliance";
+export * from "./requestApproval";
+export * from "./research";
+export * from "./triage";
+export * from "./writeDraft";
